6889438e558a35fc70fe57aaaa01618270979b00
joseph <josephPzPf@gmail.com>
2023-03-19 13:16:51 -0500

    changing framework

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import morgan from 'morgan'
+
+const app = express()
+
+app.set('port', process.env.PORT || 3001)
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
+app.use(morgan('dev'))
+
+
+
+app.listen(app.get('port'), () => {
+    console.log(`Server listening on http://localhost:${app.get('port')}`)
+})

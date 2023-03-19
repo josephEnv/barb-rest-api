aaf2a48185dca5c4b150a17b768eb3b66de28e75
joseph <josephPzPf@gmail.com>
2023-03-18 21:26:11 -0500

    any changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:3000/test'),
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
